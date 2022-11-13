577aa07c8cc1e9c9881fa40c012d7f9663569798
Alexander Prikhodko <alaront.work@gmail.com>
2022-11-13 14:50:50 +0300

    header adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="flex h-min-[100%] content">
     <header-components
-      class="w-[25%] max-w-[310px] bg-[#FFFFFF] py-[23px] px-[12px] box-border border-r-[1px] border-r-[#ada9a9] border-r-solid mr-[23px]"
+      class="w-[310px] bg-[#FFFFFF] py-[23px] px-[12px] box-border border-r-[1px] border-r-[#ada9a9] border-r-solid mr-[23px] h-[100vh] top-[0] left-[-310px] z-2"
+      :class="{
+        fixed: windowWidth <= 1200,
+        'top-[0] left-[0px]': openMenu,
+      }"
     />
     <div class="content grow-[1] pt-[25px] pr-[23px] box-border">
       <router-view></router-view>
     </div>
+    <button
+      class="fixed bottom-[10px] left-[50%] translate-x-[-50%] block bg-[#775da670] font-medium text-[16px] leading-[19px] py-[9px] px-[12px] rounded-[12px] text-[#202020]"
+      v-on:click="() => (openMenu = !openMenu)"
+      :class="windowWidth <= 1200 ? 'block' : 'hidden'"
+    >
+      {{ openMenu ? "CLOSE MENU" : "OPEN MENU" }}
+    </button>
   </div>
 </template>
 
@@ -16,6 +27,20 @@ export default {
   name: "App",
   components: {
     "header-components": Header,
+  },
+  data() {
+    return {
+      openMenu: true,
+      windowWidth: window.innerWidth,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.updateWidth);
+  },
+  methods: {
+    updateWidth() {
+      this.windowWidth = window.innerWidth;
+    },
   },
 };
 </script>
