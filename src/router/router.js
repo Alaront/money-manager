cc9161f2f462edc15b6d4cac08592ce640805391
Alexander Prikhodko <alaront.work@gmail.com>
2022-12-29 18:13:14 +0300

    protection routes

--- src/router/router.js
+++ src/router/router.js
@@ -4,14 +4,18 @@ import CategoryPage from "@/pages/CategoryPage";
 import ChangeCashPage from "@/pages/ChangeCashPage";
 import StatisticsPage from "@/pages/StatisticsPage";
 import LoginPage from "@/pages/LoginPage";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
       component: HomePage,
       alias: "/",
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/login",
@@ -23,18 +27,50 @@ export default createRouter({
       path: "/category",
       component: CategoryPage,
       alias: "/category",
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/changeCash",
       component: ChangeCashPage,
       alias: "/changeCash",
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/statistics",
       component: StatisticsPage,
       alias: "/statistics",
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
   linkActiveClass: "active-link",
   linkExactActiveClass: "active-link",
 });
+
+const getCurrentUser = () => {
+  return new Promise((resolve) => {
+    const removeListener = onAuthStateChanged(getAuth(), (user) => {
+      removeListener();
+      resolve(user);
+    });
+  });
+};
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
